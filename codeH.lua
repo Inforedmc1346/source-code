185cc7ea1bad777b28eac7fbc2278c8493f2b459
Inforedmc1346 <90752966+Inforedmc1346@users.noreply.github.com>
2023-08-05 10:16:00 +0700

    Update codeH.lua

--- codeH.lua
+++ codeH.lua
@@ -5267,6 +5267,34 @@ spawn(function()
     end
 end)
 
+SettingTab:AddToggle({
+	Name = "White Screen",
+	Default = false,
+	Callback = function(Value)
+		_G.WhiteScreen = Value
+		if _G.WhiteScreen == true then
+    game:GetService("RunService"):Set3dRenderingEnabled(false)
+elseif _G.WhiteScreen == false then
+    game:GetService("RunService"):Set3dRenderingEnabled(true)
+end
+	end    
+})
+
+SettingTab:AddToggle({
+	Name = "Remove Fog",
+	Default = false,
+	Callback = function(Value)
+		RemoveFog = Value
+		        if not RemoveFog then return end
+        while RemoveFog do wait()
+            game.Lighting.FogEnd = 9e9
+            if not RemoveFog then
+                game.Lighting.FogEnd = 2500
+            end
+        end
+	end    
+})
+
 local SetPosTab = Window:MakeTab({
 	Name = "Set Position",
 	Icon = "rbxassetid://14161592006",
@@ -6872,6 +6900,50 @@ SHTab:AddButton({
   	end    
 })
 
+SHTab:AddButton({
+	Name = "Buy Buso Haki",
+	Callback = function()
+      		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyHaki","Buso")
+  	end    
+})
+
+SHTab:AddButton({
+	Name = "Buy Geppo",
+	Callback = function()
+      		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyHaki","Geppo")
+  	end    
+})
+
+SHTab:AddButton({
+	Name = "Buy Soru",
+	Callback = function()
+      		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyHaki","Soru")
+  	end    
+})
+
+SHTab:AddButton({
+	Name = "Buy Ken(Observation)",
+	Callback = function()
+      		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("KenTalk","Buy")
+  	end    
+})
+
+SHTab:AddButton({
+	Name = "Reset Stats",
+	Callback = function()
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BlackbeardReward","Refund","1")
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BlackbeardReward","Refund","2")
+  	end    
+})
+
+SHTab:AddButton({
+	Name = "Race Reroll",
+	Callback = function()
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BlackbeardReward","Reroll","1")
+	game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BlackbeardReward","Reroll","2")
+  	end    
+})
+
 local STTab = Window:MakeTab({
 	Name = "Stats",
 	Icon = "rbxassetid://14161592006",
@@ -7097,5 +7169,55 @@ MiscTab:AddToggle({
 			end
 		end)
 		
+MiscTab:AddButton({
+	Name = "Fps Boost",
+	Callback = function()
+        pcall(function()
+            game:GetService("Lighting").FantasySky:Destroy()
+            local g = game
+            local w = g.Workspace
+            local l = g.Lighting
+            local t = w.Terrain
+            t.WaterWaveSize = 0
+            t.WaterWaveSpeed = 0
+            t.WaterReflectance = 0
+            t.WaterTransparency = 0
+            l.GlobalShadows = false
+            l.FogEnd = 9e9
+            l.Brightness = 0
+            settings().Rendering.QualityLevel = "Level01"
+            for i, v in pairs(g:GetDescendants()) do
+                if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
+                    v.Material = "Plastic"
+                    v.Reflectance = 0
+                elseif v:IsA("Decal") or v:IsA("Texture") then
+                    v.Transparency = 1
+                elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+                    v.Lifetime = NumberRange.new(0)
+                elseif v:IsA("Explosion") then
+                    v.BlastPressure = 1
+                    v.BlastRadius = 1
+                elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+                    v.Enabled = false
+                elseif v:IsA("MeshPart") then
+                    v.Material = "Plastic"
+                    v.Reflectance = 0
+                    v.TextureID = 10385902758728957
+                end
+            end
+            for i, e in pairs(l:GetChildren()) do
+                if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+                    e.Enabled = false
+                end
+            end
+            for i, v in pairs(game:GetService("Workspace").Camera:GetDescendants()) do
+                if v.Name == ("Water;") then
+                    v.Transparency = 1
+                    v.Material = "Plastic"
+                end
+            end
+        end)
+  	end    
+})
 
 OrionLib:Init()
